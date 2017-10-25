f08759acef704ac1dabc83bcb793900eef7afc2d
Maxime Moreillon <m.moreillon@gmail.com>
2017-10-25 18:59:05 +0900

    Commented config file

--- sample_scale_config.h
+++ sample_scale_config.h
@@ -4,5 +4,5 @@
 
 #define MAPPING_RAW_LOW  8076700.00
 #define MAPPING_RAW_HIGH 9700000.00
-#define MAPPING_HG_LOW  0.00
-#define MAPPING_HG_HIGH 770.0
+#define MAPPING_HG_LOW  0.00 // in hectogram
+#define MAPPING_HG_HIGH 770.0 // in hectogram

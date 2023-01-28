e1322f1520974cffd04a13ed94f2df517bb784b2
Maxime Moreillon <m.moreillon@gmail.com>
2023-01-28 09:22:05 +0900

    now using iot kernel

--- wifi.ino
+++
@@ -1,8 +0,0 @@
-void wifi_setup() {
-  Serial.print("[Wifi] connecting to: ");
-  Serial.println(WIFI_SSID);
-  
-  WiFi.hostname(HOSTNAME);
-  WiFi.mode(WIFI_STA);
-  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
-}
